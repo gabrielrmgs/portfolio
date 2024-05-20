be5c284886f5c646f54b64016915eb623c1bd2bc
gabrielrmgs <gabrielrdes@outlook.com>
2024-05-20 20:58:05 -0300

    finalizando

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-/* let btnApi = document.getElementById("projeto-api");
+let btnApi = document.getElementById("projeto-api");
 btnApi.addEventListener("click", videoapi);
 function videoapi() {
-    if (document.getElementById("print-api").style.display === "none") {
-        document.getElementById("print-api").style.display = "block"
-    } else {
-        document.getElementById("print-api").style.display = "none"
-    }
+    if (document.getElementById("print-api-div").style.display === "none") {
+        document.getElementById("print-api-div").style.display = "flex"
+        document.getElementById("print-api-div").style.alignItems = "center";
+        document.getElementById("print-api-div").style.justifyContent = "center";
 
-} */
+    } else {
+        document.getElementById("print-api-div").style.display = "none"
+    }
+}
